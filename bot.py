2b15981e80240f0375c69e394e5e38feb8108663
Likeya <shpanda91@gmail.com>
2025-03-08 11:16:50 +0700

    add menu button

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
 from get_weather import get_weather
 
 from configuration import Configuration
@@ -16,26 +18,37 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
+# Set all commands
+async def set_commands():
+    commands = [BotCommand(command='start', description='Старт'),
+                BotCommand(command='weather', description='Погода'),
+                BotCommand(command='help', description='Помощь'),
+                BotCommand(command='about', description='О разработчике')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
 
+# Command processing
 @dp.message(Command('start', 'help'))
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("/weather - Погода в Новокузнецке \n ")
+    await message.reply("/weather - Погода в Новокузнецке \n"
+                        "/help - Список доступных команд \n"
+                        "/about - Информация о разработчике \n")
+
+
+@dp.message(Command('about'))
+async def send_about(message: types.Message):
+    await message.reply("Разработчик бота: Конева Вера Александровна\n"
+                        "Telegram: @vera_koneva\n")
 
 @dp.message(Command('weather'))
 async def send_weather(message: types.Message):
-
     await message.reply(get_weather())
 
-@dp.message()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, "Your text: " + msg.text)
 
-    # Запуск процесса поллинга новых апдейтов
+# Starting the polling process for new updates
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+    await set_commands()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- get_weather.py
+++ get_weather.py
@@ -2,10 +2,10 @@ import requests
 from configuration import Configuration
 
 confjson = Configuration.load_json('config.json')
-
+key = confjson.key
 
 def get_weather():
-    key = confjson.key
+
     city_id = confjson.cityId
     url = 'https://api.openweathermap.org/data/2.5/weather'
     r = requests.get(url, params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': key})
